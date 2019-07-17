67ac273b2ae0501913b57348aea34783ab20eef3
Michael Smallegan <michael.smallegan@colorado.edu>
2019-07-17 15:47:40 -0600

    genebody coverage qc step is taking too long:

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -p short
+#SBATCH -p long
 #SBATCH --job-name=ftc-rna-seq-nextflow
 #SBATCH --mail-type=END,FAIL            # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=michael.smallegan@colorado.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=6gb                 # Memory limit
-#SBATCH --time=24:00:00            # Time limit hrs:min:sec
+#SBATCH --time=100:00:00            # Time limit hrs:min:sec
 #SBATCH --output=./log/nextflow.out
 #SBATCH --error=./log/nextflow.err
 
@@ -21,6 +21,7 @@ export PATH=/opt/singularity/3.1.1/bin:$PATH
 nextflow run nf-core/rnaseq -r 1.3 \
 -resume \
 --reads 'fastq/*{_read1,_read2}.fastq.gz' \
+--skip_genebody_coverage \
 --genome GRCm38 \
 --email michael.smallegan@colorado.edu \
 --igenomes_base '/scratch/Shares/rinn/Michael/genomes/references/' \
