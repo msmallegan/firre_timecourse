243622308b784c288f1cc6b8257fb0d82d2a8b86
Michael Smallegan <michael.smallegan@colorado.edu>
2019-10-25 14:52:43 -0600

    Updating the version of the NFCore RNAseq pipeline and the annotation to Gencode vM23

--- run.sh
+++ run.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 #SBATCH -p long
-#SBATCH --job-name=ftc-rna-seq-nextflow
-#SBATCH --mail-type=END,FAIL            # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --job-name=ftc-rnaseq
+#SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=michael.smallegan@colorado.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=6gb                 # Memory limit
-#SBATCH --time=100:00:00            # Time limit hrs:min:sec
-#SBATCH --output=./log/nextflow.out
-#SBATCH --error=./log/nextflow.err
-
-
+#SBATCH --mem=6gb
+#SBATCH --time=100:00:00
+#SBATCH --output=nextflow.out
+#SBATCH --error=nextflow.err
 
 pwd; hostname; date
-
 echo "You've requested $SLURM_CPUS_ON_NODE core."
 
-export PATH=/opt/singularity/3.1.1/bin:$PATH
+module load singularity/3.1.1
 
-nextflow run nf-core/rnaseq -r 1.3 \
+nextflow run nf-core/rnaseq -r 1.4.2 \
 -resume \
 --reads 'fastq/*{_read1,_read2}.fastq.gz' \
---skip_genebody_coverage \
---saveAlignedIntermediates \
---genome GRCm38 \
+--fasta ../genomes/references/Mus_musculus/Gencode/M23/sequence/GRCm38.p6.genome.fa \
+--gtf ../genomes/references/Mus_musculus/Gencode/M23/annotation/gencode.vM23.annotation.gtf \
+--pseudo_aligner salmon \
+--gencode \
 --email michael.smallegan@colorado.edu \
---igenomes_base '/scratch/Shares/rinn/Michael/genomes/references/' \
 -c nextflow.config
 
 date
