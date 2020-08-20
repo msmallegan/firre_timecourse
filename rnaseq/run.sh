3f0979b440bab96efc7fbb5bb751bb274af6e80d
Michael Smallegan <michael.smallegan@colorado.edu>
2020-08-20 13:37:43 -0600

    Updating the pipeline to the newest annotation and version from nf-core

--- rnaseq/run.sh
+++ rnaseq/run.sh
@@ -15,11 +15,12 @@ echo "You've requested $SLURM_CPUS_ON_NODE core."
 
 module load singularity/3.1.1
 
-nextflow run nf-core/rnaseq -r 1.4.1 \
+nextflow run nf-core/rnaseq -r 1.4.2 \
 -resume \
+-profile singularity \
 --reads 'fastq/*{_read1,_read2}.fastq.gz' \
---fasta ../genomes/references/Mus_musculus/Gencode/M23/sequence/GRCm38.p6.genome.fa \
---gtf ../genomes/references/Mus_musculus/Gencode/M23/annotation/gencode.vM23.annotation.gtf \
+--fasta ../../../genomes/Mus_musculus/Gencode/M25/GRCm38.p6.genome.fa \
+--gtf ../../../genomes/Mus_musculus/Gencode/M25/gencode.vM25.annotation.gtf \
 --pseudo_aligner salmon \
 --gencode \
 --email michael.smallegan@colorado.edu \
