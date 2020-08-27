9ff0b90ddbf5444050d151b4af363094c8f84e1d
Michael Smallegan <michael.smallegan@colorado.edu>
2020-08-26 20:34:45 -0600

    Adding a function specific to the Dox control.

--- analysis/util/_util.R
+++ analysis/util/_util.R
@@ -67,7 +67,7 @@ run_timecourse_deseq <- function(experiment,
   for(i in 2:length(res_names)) {
     tmp_res <- results(dds, 
                        name = res_names[i],
-                       independentFiltering=FALSE) %>% 
+                       independentFiltering=independent_filtering) %>% 
       as.data.frame() %>%
       rownames_to_column(var = "gene_id") %>%
       merge(g2s) %>%
@@ -90,6 +90,119 @@ run_timecourse_deseq <- function(experiment,
              fixed = T),"comparison"] <- "dynamic_firre_induction_vs_control"
   
   return(res)
+}
+
+run_control_deseq <- function(experiment,
+                                 counts, samples, genes,
+                                 design_formula,  
+                                 reduced_formula, 
+                                 independent_filtering = TRUE,
+                                 ncores = 12,
+                                 save_dds = TRUE,
+                                 save_res = TRUE) {
+  
+  ct <- unlist(strsplit(experiment, "_"))[[1]]
+  fko <- unlist(strsplit(experiment, "_"))[[2]]
+  
+  samples <- samples %>% filter(cell_type == ct,
+                                firre_ko == fko)
+  
+  counts <- counts[,samples$sample_id]
+  mode(counts) <- "integer"
+  
+  # Reorder gencode genes
+  genes <- genes[rownames(counts)]
+  
+  # Check ordering
+  stopifnot(all(rownames(samples) == colnames(counts)))
+  stopifnot(all(names(genes) == rownames(counts)))
+  
+  dds <- DESeqDataSetFromMatrix(countData = counts, 
+                                colData = samples, 
+                                design = design_formula,
+                                rowData = genes)
+  
+  # Filter low counts
+  dds <- dds[rowSums(counts(dds)) >= 100,]
+  
+  
+  dds <- DESeq(dds, 
+               test="LRT", 
+               reduced = reduced_formula,
+               parallel=TRUE,
+               BPPARAM=MulticoreParam(ncores))
+  
+  if(save_dds == TRUE) {
+    saveRDS(dds, paste0("results/dox_control_",
+                        ct, "_",
+                        fko, "_dds.rds"))
+  }
+  
+  # Compile results
+  res_names <- resultsNames(dds)
+  res <- results(dds, 
+                 name = res_names[2],
+                 independentFiltering=independent_filtering)
+  
+  res_shrunken <- lfcShrink(dds = dds,
+                            coef = res_names[2],
+                            res = res,
+                            type = "apeglm",
+                            parallel = TRUE,
+                            BPPARAM=MulticoreParam(ncores)) %>% 
+    as.data.frame() %>%
+    rownames_to_column(var = "gene_id") %>%
+    merge(g2s) %>%
+    mutate(result_name = res_names[2]) 
+
+  if(save_res == TRUE) {
+    saveRDS(res, paste0("results/dox_control_",
+                        ct, "_",
+                        fko, "_res.rds"))
+  }
+  
+  # Convert to data frame
+  res <- res %>% 
+    as.data.frame() %>%
+    rownames_to_column(var = "gene_id") %>%
+    merge(g2s) %>%
+    mutate(result_name = res_names[2]) 
+  
+  for(i in 3:length(res_names)) {
+    tmp_res <- results(dds, 
+                       name = res_names[i],
+                       independentFiltering=independent_filtering)
+    
+    tmp_res_shrunken <- suppressMessages(lfcShrink(dds = dds,
+                              coef = res_names[i],
+                              res = tmp_res,
+                              type = "apeglm",
+                              parallel = TRUE,
+                              BPPARAM=MulticoreParam(ncores))) %>%
+      as.data.frame() %>%
+      rownames_to_column(var = "gene_id") %>%
+      merge(g2s) %>%
+      mutate(result_name = res_names[i]) 
+    
+    tmp_res <- tmp_res %>% 
+      as.data.frame() %>%
+      rownames_to_column(var = "gene_id") %>%
+      merge(g2s) %>%
+      mutate(result_name = res_names[i]) 
+    
+    res_shrunken <- bind_rows(res_shrunken, tmp_res_shrunken)
+    res <- bind_rows(res, tmp_res)
+  }
+  
+  # Label results
+  res_shrunken$cell_type <- ct
+  res_shrunken$firre_ko <- fko
+  res$cell_type <- ct
+  res$firre_ko <- fko
+  
+  combined_results <- list("res" = res, "res_shrunken" = res_shrunken)
+  
+  return(combined_results)
 }
 
 get_dynamic_comparison_df <- function(res) {
