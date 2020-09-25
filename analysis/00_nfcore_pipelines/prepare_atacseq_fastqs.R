7cb2368d4d436853d95b6073cf02fe954c1b41e9
Michael Smallegan <michael.smallegan@colorado.edu>
2020-09-25 11:29:36 -0600

    Adding atacseq fastq script and updating rnaseq fastq script for new filenames

--- analysis/00_nfcore_pipelines/prepare_atacseq_fastqs.R
+++ analysis/00_nfcore_pipelines/prepare_atacseq_fastqs.R
@@ -1,31 +1,31 @@
 library(tidyverse)
 # Create a script to move files to scratch
-if(!dir.exists("../../rnaseq/fastq")) {
+if(!dir.exists("../../atacseq/fastq")) {
   cat("Creating fastq directory") 
-  dir.create("../../rnaseq/fastq")
+  dir.create("../../atacseq/fastq")
 }
 
-samples <- read.csv("../../rnaseq/rnaseq_samplesheet.csv")
+samples <- read.csv("../../atacseq/atacseq_samplesheet.csv")
 samples$dest_read1 <- paste0(file.path("/scratch/Shares/rinn/Michael",
-                                       "firre_timecourse/rnaseq/fastq/"),
+                                       "firre_timecourse/atacseq/fastq/"),
                              samples$sample_id, "_read1.fastq.gz")
 samples$dest_read2 <- paste0(file.path("/scratch/Shares/rinn/Michael",
-                                       "firre_timecourse/rnaseq/fastq/"),
+                                       "firre_timecourse/atacseq/fastq/"),
                              samples$sample_id, "_read2.fastq.gz")
 
 cp_read1 <- samples %>% dplyr::select(raw_data_read1,
                                       dest_read1)
 names(cp_read1) <- c("src", "dest")
 cp_read2 <- samples %>% dplyr::select(raw_data_read2,
-                                       dest_read2)
+                                      dest_read2)
 names(cp_read2) <- c("src", "dest")
 cp_df <- bind_rows(cp_read1, cp_read2)
 cp_df$cmd <- "cp"
 cp_df <- cp_df %>% dplyr::select(cmd, src, dest)
 
 # Write script to move FASTQs to scratch
-cat("#!/bin/bash \n", file = "copy_rnaseq_fastqs_to_scratch.sh")
-write.table(cp_df, "copy_rnaseq_fastqs_to_scratch.sh", append = TRUE,
+cat("#!/bin/bash \n", file = "copy_atacseq_fastqs_to_scratch.sh")
+write.table(cp_df, "copy_atacseq_fastqs_to_scratch.sh", append = TRUE,
             sep = " ", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
 ## Write a file to check MD5 sums
@@ -36,7 +36,7 @@ md5_read2 <- samples %>%
   dplyr::select(md5_read2, dest_read2)
 names(md5_read2) <- c("md5sum", "file")
 md5_df <- bind_rows(md5_read1, md5_read2)
-write.table(md5_df, "rnaseq_md5_list.txt",
+write.table(md5_df, "atacseq_md5_list.txt",
             quote = FALSE, row.names = FALSE, col.names = FALSE, sep = " ")
 
 
