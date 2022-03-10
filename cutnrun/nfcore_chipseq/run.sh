a21c1eccadc6f2fc4a93024e948b0eb8775a2fb7
Michael Smallegan <michael.smallegan@colorado.edu>
2022-03-10 16:54:01 -0700

    Atac-seq timing

--- cutnrun/nfcore_chipseq/run.sh
+++ cutnrun/nfcore_chipseq/run.sh
@@ -14,13 +14,14 @@ pwd; hostname; date
 
 module load singularity/3.1.1
 
-nextflow run nf-core/chipseq -r 1.2.1 \
+nextflow run nf-core/cutandrun -r 1.1 \
 -profile singularity \
 --input design.csv \
---fasta /scratch/Shares/rinn/genomes/Mus_musculus/Gencode/M25/GRCm38.primary_assembly.genome.fa \
---gtf /scratch/Shares/rinn/genomes/Mus_musculus/Gencode/M25/gencode.vM25.primary_assembly.annotation.gtf \
+--fasta /scratch/Shares/rinn/genomes/Mus_musculus/Gencode/M25/GRCm38.p6.genome.fa \
+--gtf /scratch/Shares/rinn/genomes/Mus_musculus/Gencode/M25/gencode.vM25.annotation.gtf \
 --macs_gsize 1.87e9 \
 --blacklist mm10-blacklist.v2.bed \
+--igg_control false \
 --email michael.smallegan@colorado.edu \
 -resume \
 -c nextflow.config
