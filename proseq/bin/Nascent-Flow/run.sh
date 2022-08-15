f27456ad1603cb1c23af96a59b9f338f321ba6bd
Michael Smallegan <michael.smallegan@colorado.edu>
2022-08-15 07:55:06 -0600

    Adding the conda environment with rseqc installed.

--- proseq/bin/Nascent-Flow/run.sh
+++ proseq/bin/Nascent-Flow/run.sh
@@ -24,9 +24,12 @@ module load bedtools/2.28.0
 module load openmpi/1.6.4
 module load gcc/7.1.0
 
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate rseqc
+
 #activate the virtual machine
 #source /Users/allenma/Nexflow_pipelines/bin/activate
 
 
-nextflow run main.nf -resume -profile 'mm10' --workdir '/scratch/Shares/rinn/Michael/firre_timecourse/proseq/work' --genome_id 'mm10' --outdir '/scratch/Shares/rinn/Michael/firre_timecourse/proseq/results' --email mism6893@colorado.edu --fastqs '/scratch/Shares/rinn/Michael/firre_timecourse/proseq/fastq/*_{R1,R2}.fastq.gz' --saveBAM --skipFastQC --noTrim
+nextflow run main.nf -resume -profile 'mm10' --workdir '/scratch/Shares/rinn/Michael/firre_timecourse/proseq/work' --genome_id 'mm10' --outdir '/scratch/Shares/rinn/Michael/firre_timecourse/proseq/results' --email mism6893@colorado.edu --fastqs '/scratch/Shares/rinn/Michael/firre_timecourse/proseq/fastq/*_{R1,R2}.fastq.gz' --saveBAM
 
